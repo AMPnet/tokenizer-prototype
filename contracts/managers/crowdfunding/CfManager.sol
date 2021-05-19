082d3f29248bc374df0098b636a1a0df7c141756
Filip Dujmusic <38497167+filipdujmusic@users.noreply.github.com>
2021-05-19 20:32:07 +0200

    Cancel investment implementation (#4)
    Implemented cancel investment feature
    
    Implemented campaign finalization procedure

--- contracts/managers/crowdfunding/CfManager.sol
+++ contracts/managers/crowdfunding/CfManager.sol
@@ -2,17 +2,20 @@
 pragma solidity ^0.8.0;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IAsset } from "../../asset/IAsset.sol";
 import { ICfManager } from "../crowdfunding/ICfManager.sol";
 import { AssetState } from "../../shared/Enums.sol";
 
 contract CfManager is ICfManager {
+    using SafeERC20 for IERC20;
 
     address public owner;
     IAsset public asset;
     uint256 public minInvestment;
     uint256 public maxInvestment;
     uint256 public endsAt;
+    bool public finalized;
 
     constructor(address _owner, uint256 _minInvestment, uint256 _maxInvestment, uint256 _endsAt) {
         require(
@@ -33,6 +36,38 @@ contract CfManager is ICfManager {
         endsAt = _endsAt;
     }
 
+    modifier onlyOwner(address _wallet) {
+        require(
+            _wallet == owner,
+            "Only owner can call this function."
+        );
+        _;
+    }
+
+    modifier assetInitialized() {
+        require(
+            address(asset) != address(0),
+            "Asset address not set."
+        );
+        _;
+    }
+
+    modifier notFinalized() {
+        require(
+            !finalized,
+            "The campaign is not finalized."
+        );
+        _;
+    }
+
+    modifier notExpired() {
+        require(
+            block.timestamp <= endsAt,
+            "The campaign has expired."
+        );
+        _;
+    }
+
     function setAsset(address _assetAddress) override external {
         IAsset _asset = IAsset(_assetAddress);
         require(
@@ -46,11 +81,7 @@ contract CfManager is ICfManager {
         asset = _asset;
     }
 
-    function invest(uint256 amount) external {
-        require(
-            address(asset) != address(0),
-            "Asset address not set."
-        );
+    function invest(uint256 amount) external assetInitialized notExpired {
         IERC20 assetToken = IERC20(address(asset));
         IERC20 stablecoin = IERC20(asset.issuer().stablecoin());
 
@@ -71,10 +102,29 @@ contract CfManager is ICfManager {
             "Investment amount too high."
         );
 
-        stablecoin.transferFrom(msg.sender, address(this), amount);
-        if (asset.addShareholder(msg.sender, amount)) {
-            asset.setCreator(owner);
-        }
+        stablecoin.safeTransferFrom(msg.sender, address(this), amount);
+        asset.addShareholder(msg.sender, amount);
+    }
+
+    function cancelInvestment() external assetInitialized notFinalized {
+        IERC20 stablecoin = IERC20(asset.issuer().stablecoin());
+        uint256 shares = IERC20(address(asset)).balanceOf(msg.sender);
+        require(
+            shares > 0,
+            "No shares owned."
+        );
+        asset.removeShareholder(msg.sender, shares);
+        stablecoin.safeTransfer(msg.sender, shares);
+    }
+
+    function finalize() external onlyOwner(msg.sender) assetInitialized notExpired notFinalized {
+        require(
+            IERC20(address(asset)).balanceOf(address(this)) == 0,
+            "Can only finalize fully funded Assets."
+        );
+        asset.finalize();
+        asset.setCreator(owner);
+        finalized = true;
     }
 
 }
