a84e8152cd8d7ab6c958189701ffe20ff72c8c8a
Filip Dujmusic <filip@ampnet.io>
2021-09-29 10:04:30 +0200

    v++

--- contracts/asset-simple/AssetSimpleFactory.sol
+++ contracts/asset-simple/AssetSimpleFactory.sol
@@ -9,7 +9,7 @@ import "../registry/INameRegistry.sol";
 contract AssetSimpleFactory is IAssetSimpleFactory {
     
     string constant public FLAVOR = "AssetSimpleV1";
-    string constant public VERSION = "1.0.15";
+    string constant public VERSION = "1.0.16";
 
     address[] public instances;
     mapping (address => address[]) instancesPerIssuer;
