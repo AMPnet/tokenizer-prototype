795a6003fe842788ddfe69a135e6fb4a5e796cf9
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-02-02 15:17:29 +0100

    SD-527: use OpenZeppelin Merkle validator

--- contracts/services/MerkleTreePathValidator.sol
+++ contracts/services/MerkleTreePathValidator.sol
@@ -1,24 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 contract MerkleTreePathValidator {
 
-    struct PathSegment {
-        bytes32 siblingHash;
-        bool isLeft;
-    }
+    using MerkleProof for bytes32[];
 
-    function containsNode(bytes32 merkleTreeRoot, address wallet, uint256 balance, PathSegment[] calldata path) external pure returns (bool) {
-        bytes32 currentHash = keccak256(abi.encode(wallet, balance));
-
-        for (uint i = 0; i < path.length; i++) {
-            if (path[i].isLeft) {
-                currentHash = keccak256(abi.encode(path[i].siblingHash, currentHash));
-            } else {
-                currentHash = keccak256(abi.encode(currentHash, path[i].siblingHash));
-            }
-        }
-
-        return currentHash == merkleTreeRoot;
+    function containsNode(
+        bytes32 merkleTreeRoot,
+        uint256 treeDepth,
+        address wallet,
+        uint256 balance,
+        bytes32[] memory proof
+    ) external pure returns (bool) {
+        if (proof.length != treeDepth) return false;
+        bytes32 leaf = keccak256(abi.encode(wallet, balance));
+        return proof.verify(merkleTreeRoot, leaf);
     }
 }
