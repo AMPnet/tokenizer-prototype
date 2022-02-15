9ef94e10c5b1abce2640ecdccf7a5d58f1e0ffbd
Filip Dujmusic <filip@ampnet.io>
2022-02-15 17:12:16 +0100

    remove deprecated PayoutManager contracts

--- contracts/managers/snapshot-distributor/IERC20Snapshot.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IERC20Snapshot {
-    function snapshot() external returns (uint256);
-    function totalSupplyAt(uint256 snapshotId) external view returns (uint256); 
-    function balanceOfAt(address account, uint256 snapshotId) external view returns (uint256);
-}
