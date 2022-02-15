aaf3c4a830bea1397f3e23daa138f09fc67a4c44
Filip Dujmusic <filip@ampnet.io>
2022-02-15 17:13:27 +0100

    Refactor: Merge FaucetService and IFaucetService in a single file just like in other Services

--- contracts/services/FaucetService.sol
+++ contracts/services/FaucetService.sol
@@ -2,7 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "../shared/IVersioned.sol";
-import "./IFaucetService.sol";
+
+interface IFaucetService {
+    function faucet(address payable[] calldata _wallets) external;
+    function updateRewardAmount(uint256 _newRewardAmount) external;
+    function updateBalanceThresholdForReward(uint256 _newBalanceThresholdForReward) external;
+    function updateCallerStatus(address _caller, bool _approved) external;
+    function transferOwnership(address _newOwner) external;
+    receive() external payable;
+    function release() external;
+}
 
 contract FaucetService is IVersioned, IFaucetService {
 
